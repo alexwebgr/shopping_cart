82ba49bca86eb63027325facc502b0c1719b7702
alexwebgr <websurfer992@gmail.com>
2020-12-10 14:16:40 +0200

    removed unused deps

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,9 +19,6 @@ SimpleCov.start do
 end
 
 RSpec.configure do |config|
-  config.before(:suite) do
-    FactoryBot.find_definitions
-  end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
